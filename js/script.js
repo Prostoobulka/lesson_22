02027b275d1f669a55e96316238e1798652e9065
Prostoobulka <94324870+Prostoobulka@users.noreply.github.com>
2022-11-26 00:53:55 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,82 @@
+"use strict"
+/* */
+// Завдання №1
+/*
+Варіант - 1
+console.log("I'm")
+console.log("Vlad")
+
+Варіант - 2
+console.log("I'm") console.log("Vlad")
+
+Варіант - 3
+console.log("I'm);
+console.log("Vlad");
+*/
+/*
+Style lowelCamelCase
+
+let eggsPig;
+let $age;
+let __age;
+
+let age-;
+let #age;
+let 1312312age;
+
+*/
+// Константа
+// const BLACK_COLOR = "#000";
+
+
+/* Змінна значення
+let age = 20;
+console.log(age);
+age = 21;
+console.log(age);
+
+let age = 20;
+let age2;
+console.log(age);
+age = 21;
+age2 = age;
+console.log(age2);
+*/
+
+// Типи даних
+/* Boolean
+let something = false;
+if (something) {
+	console.log(something);
+} else {
+	console.log("false - 1");
+}
+*/
+
+// console.log(something);
+// console.log(typeof something);
+/*
+let something = 20 * "Vlad";
+console.log(something); - NaN
+console.log(typeof something); - Number
+ */
+
+/* Number
+let something = 10 / 0;
+console.log(something); - Number
+console.log(typeof something) - Infinity, -Infinity
+*/
+
+/*
+let something = 12123123123123123123123121231n;
+console.log(typeof something);
+
+let something = "20" * "20";
+console.log(something);
+console.log(typeof something);
+ */
+
+// Завдання №2
+
+
+
